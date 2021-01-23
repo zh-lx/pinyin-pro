732f735230e4baffafd87721d5fe66f694b33ec2
zh-lx <18366276315@163.com>
2021-01-23 18:49:25 +0800

    feat(package): 减少npm包体积
    减少与使用无关文件的上传

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 export declare const pinyin: (
   word: string,
   options?: {
-    toneType: 'symbol' | 'num' | 'none'; // 音调样式
-    pattern: 'pinyin' | 'initial' | 'final'; // 返回数据模式
-    type: 'string' | 'array'; // 返回数据类型
-    multiple: boolean; // 是否返回多音，仅在单字有效
+    toneType?: 'symbol' | 'num' | 'none'; // 音调样式
+    pattern?: 'pinyin' | 'initial' | 'final'; // 返回数据模式
+    type?: 'string' | 'array'; // 返回数据类型
+    multiple?: boolean; // 是否返回多音，仅在单字有效
   }
 ) => string | string[];
