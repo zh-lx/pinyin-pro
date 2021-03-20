e18e9a80e6eea3e4a2512734b6fd63b4e8ff691f
zh-lx <18366276315@163.com>
2021-03-20 20:32:11 +0800

    fix: 修复index.d.ts pattern缺失num可选项的问题

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ export declare const pinyin: (
   word: string,
   options?: {
     toneType?: 'symbol' | 'num' | 'none'; // 音调样式
-    pattern?: 'pinyin' | 'initial' | 'final'; // 返回数据模式
+    pattern?: 'pinyin' | 'initial' | 'final' | 'num'; // 返回数据模式
     type?: 'string' | 'array'; // 返回数据类型
     multiple?: boolean; // 是否返回多音，仅在单字有效
   }
