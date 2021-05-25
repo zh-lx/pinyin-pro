13c171641d87318605046314f75a7a060ef974f9
zh-lx <18366276315@163.com>
2021-05-25 22:40:27 +0800

    feat: 增加单字多音未找到时的测试用例

--- test/dist.test.js
+++ test/dist.test.js
@@ -149,6 +149,11 @@ describe('multiple', () => {
     const result = pinyin('好', { multiple: true, type: 'array' });
     expect(result).to.deep.equal(['hǎo', 'hào']);
   });
+
+  it('none-find', () => {
+    const result = pinyin('a', { multiple: true, type: 'array' });
+    expect(result).to.deep.equal(['a']);
+  });
 });
 
 describe('pattern', () => {
@@ -212,5 +217,98 @@ describe('toneType', () => {
   it('symbol-array', () => {
     const result = pinyin('汉语拼音', { toneType: 'symbol', type: 'array' });
     expect(result).to.deep.equal(['hàn', 'yǔ', 'pīn', 'yīn']);
+  });
+});
+
+describe('getPinyin', () => {
+  it('double symbol', () => {
+    const result = pinyin('aaaa');
+    expect(result).to.be.equal('aaaa');
+  });
+
+  it('length greater than 5', () => {
+    const result = pinyin('赵钱孙李吧你');
+    expect(result).to.be.equal('zhào qián sūn lǐ ba nǐ');
+  });
+
+  it('dict2', () => {
+    const result = pinyin('阿比让');
+    expect(result).to.be.equal('ā bǐ ràng');
+  });
+});
+
+describe('getNumOfTone', () => {
+  it('no tone', () => {
+    const result = pinyin('赵钱孙李吧你b', { pattern: 'num' });
+    expect(result).to.be.equal('4 2 1 3 0 3 ');
+  });
+});
+
+describe('pinyinFn', () => {
+  it('not string type', () => {
+    const result = pinyin(2222);
+    expect(result).to.be.equal(2222);
+  });
+
+  it('empty string', () => {
+    const resultStr = pinyin('');
+    const resultArr = pinyin('', { type: 'array' });
+    expect(resultStr).to.be.equal('');
+    expect(resultArr).to.deep.equal([]);
+  });
+
+  it('origin', () => {
+    const result = pinyin('赵钱孙李吧');
+    expect(result).to.be.equal('zhào qián sūn lǐ ba');
+  });
+
+  it('multiple', () => {
+    const result = pinyin('好', { multiple: true });
+    expect(result).to.be.equal('hǎo hào');
+  });
+
+  it('pattern', () => {
+    const resultNumStr = pinyin('赵钱孙李吧', { pattern: 'num' });
+    expect(resultNumStr).to.be.equal('4 2 1 3 0');
+
+    const resultNumArr = pinyin('赵钱孙李吧', {
+      pattern: 'num',
+      type: 'array',
+    });
+    expect(resultNumArr).to.deep.equal(['4', '2', '1', '3', '0']);
+
+    const resultInitial = pinyin('赵钱孙李吧', {
+      pattern: 'initial',
+    });
+    expect(resultInitial).to.be.equal('zh q s l b');
+
+    const resultFinal = pinyin('赵钱孙李吧', {
+      pattern: 'final',
+    });
+    expect(resultFinal).to.be.equal('ào ián ūn ǐ a');
+
+    const resultFirst = pinyin('赵钱孙李额', {
+      pattern: 'first',
+    });
+    expect(resultFirst).to.be.equal('z q s l é');
+
+    const resultFirst2 = pinyin('赵钱孙李额', {
+      pattern: 'first',
+      toneType: 'none',
+    });
+    expect(resultFirst2).to.be.equal('z q s l e');
+  });
+
+  it('toneType', () => {
+    const resultNone = pinyin('赵钱孙李吧', { toneType: 'none' });
+    expect(resultNone).to.be.equal('zhao qian sun li ba');
+
+    const resultNum = pinyin('赵钱孙李吧', { toneType: 'num' });
+    expect(resultNum).to.be.equal('zhao4 qian2 sun1 li3 ba0');
+  });
+
+  it('array', () => {
+    const result = pinyin('赵钱孙李吧', { type: 'array' });
+    expect(result).to.deep.equal(['zhào', 'qián', 'sūn', 'lǐ', 'ba']);
   });
 });
