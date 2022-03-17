b18eae2c74e0ace6988bf46b5b60b1793063e3ef
zh-lx <18366276315@163.com>
2022-03-17 19:46:12 +0800

    fix: 修复双字节 unicode 编码的文字返回默认值不正确的问题

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,3 +3,8 @@ var regex = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
 export function getStringLength(string: string) {
   return string.replace(regex, '_').length;
 }
+
+export function isZhChar(char: string) {
+  let code = char.charCodeAt(0);
+  return code >= 19968 && code <= 40869;
+}
--- types/lib/utils.d.ts
+++ types/lib/utils.d.ts
@@ -1,1 +1,2 @@
 export declare function getStringLength(string: string): number;
+export declare function isZhChar(char: string): boolean;
