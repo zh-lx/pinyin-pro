063dd3688863841fc0550807756129b05a3e47a0
zhoulixiang <zhoulixiang@kuaishou.com>
2024-03-16 23:32:32 +0800

    feat: 数字词库增加"几"

--- lib/common/constant.ts
+++ lib/common/constant.ts
@@ -1,5 +1,6 @@
 export const DoubleUnicodePrefixReg = /^[\uD800-\uDBFF]$/;
 export const DoubleUnicodeSuffixReg = /^[\uDC00-\uDFFF]$/;
+export const DoubleUnicodeReg = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
 export enum Priority {
   DictNumber = 15,
   DICT2 = 20,
