b6f10103172eff7e232224504c52b11920396c51
zhoulixiang <zhoulixiang@kuaishou.com>
2024-05-28 19:59:40 +0800

    feat: 优化自定义 dict 的逻辑

--- test/dict.test.js
+++ test/dict.test.js
@@ -20,4 +20,44 @@ describe("addDict", () => {
     const result = pinyin("汉语拼音");
     expect(result).to.be.equal("hàn yǔ pīn yīn");
   });
+
+  it("[addDict]dict handle add", () => {
+    const stringDict = {
+      汉: ['yīn']
+    }
+    addDict(stringDict, { name: "handle-add", dict1: 'add' });
+    const result = pinyin("汉", { multiple: true });
+    expect(result).to.be.equal("hàn yīn");
+    removeDict('handle-add');
+  });
+
+  it("[addDict]dict handle replace", () => {
+    const stringDict = {
+      汉: ['yīn']
+    }
+    addDict(stringDict, { name: "handle-replace", dict1: 'replace' });
+    const result = pinyin("汉", { multiple: true });
+    expect(result).to.be.equal("yīn");
+    removeDict('handle-replace');
+  });
+
+  it("[addDict]undefined dict", () => {
+    const stringDict = {
+      䃜: 'yī'
+    }
+    addDict(stringDict, { name: "handle-new" });
+    const result = pinyin("䃜");
+    expect(result).to.be.equal("yī");
+    removeDict('handle-new');
+  });
+
+  it("[addDict]unnamed dict", () => {
+    const stringDict = {
+      䃜: 'yī'
+    }
+    addDict(stringDict);
+    const result = pinyin("䃜");
+    expect(result).to.be.equal("yī");
+    removeDict();
+  });
 });
